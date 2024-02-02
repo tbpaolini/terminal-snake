dfde8e0ccbf109df0f352e4ab65718d16dd407e6
Tiago Becerra Paolini <tpaolini@gmail.com>
2024-02-02 13:33:43 -0300

    Included system speciffic headers

--- src/includes.h
+++ src/includes.h
@@ -3,3 +3,13 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <ctype.h>
+
+#ifdef _WIN32
+#include <windows.h>
+
+#else
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+
+#endif // _WIN_32
