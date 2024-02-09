eef12e4fd4ac9c342f64cd67359a1e97b81371b0
Tiago Becerra Paolini <tpaolini@gmail.com>
2024-02-09 13:57:58 -0300

    Functions for allocating memory and resetting the terminal

--- src/game_logic.c
+++ src/game_logic.c
@@ -1,2 +1,28 @@
 #include "includes.h"
 
+// Reset the terminal and its window back to their original states
+static void cleanup()
+{
+    printf(TERM_RESET MAIN_SCREEN);
+}
+
+// Perform a clean-up before exiting
+static void clean_exit()
+{
+    cleanup();
+    exit(EXIT_SUCCESS);
+}
+
+// Allocate memory initialized to zero and check if it has been successfully allocated
+// Note: program exits on failure.
+static void* xmalloc(size_t size)
+{
+    void* ptr = calloc(1, size);
+    if (!ptr)
+    {
+        cleanup();
+        fprintf(stderr, TEXT_RED "Error:" COLOR_RESET " Not enough memory.\n");
+        quick_exit(EXIT_FAILURE);
+    }
+    return ptr;
+}
