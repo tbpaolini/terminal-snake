70b0ddf00580997b0be335efaccec08b367e7f5f
Tiago Becerra Paolini <tpaolini@gmail.com>
2024-02-09 13:35:45 -0300

    Structs and enums for keeping track of the game's state

--- src/game_logic.h
+++ src/game_logic.h
@@ -1,2 +1,29 @@
 #pragma once
 
+#include "includes.h"
+
+typedef struct GameState GameState;
+typedef struct GameCoord GameCoord;
+typedef enum SnakeDirection {UP, DOWN, RIGHT, LEFT} SnakeDirection;
+typedef enum GameArena {FOOD=-1, EMPTY=0, BLOCKED=1} GameArena;
+
+// Information needed for drawing the game
+struct GameState
+{
+    GameArena **arena;          // (2D array) Elements on each coordinate of the screen 
+    GameCoord *snake;           // (double-ended queue) Coordinates of the screen where the snake parts are being drawn
+    size_t head;                // Index of snake[] where the head is
+    size_t tail;                // Index of snake[] where the tail is
+    GameCoord position;         // Coordinate of the screen where the snake's head is
+    SnakeDirection direction;   // Direction the snake is moving to
+    uint32_t sleep_time;        // Duration of each drawn frame
+    uint64_t score;             // Player's score
+};
+
+// Coordinates on the terminal
+// Notes: 1-indexed, top left is (1,1).
+struct GameCoord
+{
+    size_t row; // Row number
+    size_t col; // Column number
+};
