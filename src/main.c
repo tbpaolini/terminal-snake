23874228aa6edaeca48c0321ec73b2220dc9bc5d
Tiago Becerra Paolini <tpaolini@gmail.com>
2024-02-11 20:12:40 -0300

    Made this project into an unity build
    This way just compiling main.c is enough for compiling the entire project.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,8 @@
+/* Note: Just compiling this 'main.c' file is enough for building the entire project. */
+
 #include "includes.h"
+#include "helper_functions.c"
+#include "game_loop.c"
 
 int main(int argc, char **argv)
 {
